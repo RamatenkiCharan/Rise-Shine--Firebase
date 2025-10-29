c434fca99dfc5995ca6720c18601457ebda4fe2e
Charan Ramatenki <ramatenkicharan1@gmail.com>
2025-10-29 17:18:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1157920603-960b9",
+  "appId": "1:789022996889:web:2a55788292e118f6b3fb08",
+  "apiKey": "AIzaSyAtYZgYXdHyDHYgkHJULG9nFaULuK7xJGI",
+  "authDomain": "studio-1157920603-960b9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "789022996889"
+};
